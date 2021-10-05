1057e8e6ed119a465b8e96984f586c4f141b6c51
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-10-05 11:44:02 -0300

    ensure AuthUseCase throw MissingParamsError if no email is provider

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -11,6 +11,6 @@ describe('AuthUseCase', () => {
   test('Should throw if no email is provider', async () => {
     const sut = new AuthUseCase()
     const promise = sut.auth()
-    expect(promise).rejects.toThrow()
+    expect(promise).rejects.toThrow(new MissingParamsError('email'))
   })
 })
