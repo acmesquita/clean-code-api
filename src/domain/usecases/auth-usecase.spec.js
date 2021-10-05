a7d04f259ec2d1081c2ad291050fed44e8ab969b
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-10-05 11:45:51 -0300

    ensure AuthUseCase throw MissingParamsError if no password is provider

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -1,9 +1,13 @@
 import { MissingParamsError } from '../../utils/errors'
 
 class AuthUseCase {
-  async auth (email) {
+  async auth (email, passowrd) {
     if (!email) {
       throw new MissingParamsError('email')
+    }
+
+    if (!passowrd) {
+      throw new MissingParamsError('password')
     }
   }
 }
@@ -12,5 +16,11 @@ describe('AuthUseCase', () => {
     const sut = new AuthUseCase()
     const promise = sut.auth()
     expect(promise).rejects.toThrow(new MissingParamsError('email'))
+  })
+
+  test('Should throw if no password is provider', async () => {
+    const sut = new AuthUseCase()
+    const promise = sut.auth('any_email@email.com')
+    expect(promise).rejects.toThrow(new MissingParamsError('password'))
   })
 })
