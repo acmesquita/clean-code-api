1bf934b0953fd53b8cd4e5a33ebd36ec7453fdda
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-10-05 12:05:03 -0300

    ensure AuthUseCase throws if loadUserByEmailRepository is invalid provider

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -1,4 +1,4 @@
-import { MissingParamsError } from '../../utils/errors'
+import { InvalidParamsError, MissingParamsError } from '../../utils/errors'
 
 class AuthUseCase {
   constructor (loadUserByEmailRepository) {
@@ -12,6 +12,14 @@ class AuthUseCase {
 
     if (!passowrd) {
       throw new MissingParamsError('password')
+    }
+
+    if (!this.loadUserByEmailRepository) {
+      throw new MissingParamsError('loadUserByEmailRepository')
+    }
+
+    if (!this.loadUserByEmailRepository.load) {
+      throw new InvalidParamsError('loadUserByEmailRepository')
     }
 
     await this.loadUserByEmailRepository.load(email)
@@ -53,5 +61,19 @@ describe('AuthUseCase', () => {
     await sut.auth('any_email@email.com', 'any_password')
 
     expect(loadUserByEmailRepositorySpy.email).toBe('any_email@email.com')
+  })
+
+  test('Should throw if no LoadUserByEmailRepository is provider', async () => {
+    const sut = new AuthUseCase()
+    const promise = sut.auth('any_email@email.com', 'any_password')
+
+    expect(promise).rejects.toThrow(new MissingParamsError('loadUserByEmailRepository'))
+  })
+
+  test('Should throw if no LoadUserByEmailRepository has no load method', async () => {
+    const sut = new AuthUseCase({})
+    const promise = sut.auth('any_email@email.com', 'any_password')
+
+    expect(promise).rejects.toThrow(new InvalidParamsError('loadUserByEmailRepository'))
   })
 })
