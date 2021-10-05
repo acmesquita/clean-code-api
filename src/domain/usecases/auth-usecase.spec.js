8c469bbfe126f1b203b6b3813531ea43a0f9bfcc
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-10-05 13:56:19 -0300

    move AuthUseCase to it's own file

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -1,34 +1,5 @@
 import { InvalidParamsError, MissingParamsError } from '../../utils/errors'
-
-class AuthUseCase {
-  constructor (loadUserByEmailRepository) {
-    this.loadUserByEmailRepository = loadUserByEmailRepository
-  }
-
-  async auth (email, passowrd) {
-    if (!email) {
-      throw new MissingParamsError('email')
-    }
-
-    if (!passowrd) {
-      throw new MissingParamsError('password')
-    }
-
-    if (!this.loadUserByEmailRepository) {
-      throw new MissingParamsError('loadUserByEmailRepository')
-    }
-
-    if (!this.loadUserByEmailRepository.load) {
-      throw new InvalidParamsError('loadUserByEmailRepository')
-    }
-
-    const user = await this.loadUserByEmailRepository.load(email)
-
-    if (!user) {
-      return null
-    }
-  }
-}
+import { AuthUseCase } from './auth-usecase'
 
 const makeSut = () => {
   class LoadUserByEmailRepositorySpy {
