70d6700d6d7d5d3a2d65b4faeeb6d9261d1b8ac2
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-10-07 11:56:12 -0300

    move LoadUserByEmailRepository to its own file

---
+++ src/infra/repositories/load-user-by-email-repository.js
@@ -0,0 +1,14 @@
+export class LoadUserByEmailRepository {
+  constructor (userModel) {
+    this.userModel = userModel
+  }
+
+  async load (email) {
+    const user = await this.userModel.findOne({ email }, {
+      projection: {
+        password: 1
+      }
+    })
+    return user
+  }
+}
