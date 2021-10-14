8d9ff4a0504bbff203448ff88f02fb53d439e1fa
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-10-14 15:13:17 -0300

    move test cors to its own file

---
+++ src/main/middlewares/cors.test.js
@@ -0,0 +1,15 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('App CORS', () => {
+  test('Should enable cors', async () => {
+    app.get('/teste_cors', (req, res) => {
+      res.send('test_header')
+    })
+    const response = await request(app).get('/teste_cors')
+
+    expect(response.headers['access-control-allow-origin']).toBe('*')
+    expect(response.headers['access-control-allow-methods']).toBe('*')
+    expect(response.headers['access-control-allow-headers']).toBe('*')
+  })
+})
