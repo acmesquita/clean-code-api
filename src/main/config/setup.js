a5d1e3772edd24e5afbb05f48b7576a21a3ccc77
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-10-14 15:11:37 -0300

    move cors to its own file

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,9 +1,6 @@
+import cors from '../middlewares/cors'
+
 export function setup (app) {
   app.disable('x-powered-by')
-  app.use((req, res, next) => {
-    res.set('access-control-allow-origin', '*')
-    res.set('access-control-allow-methods', '*')
-    res.set('access-control-allow-headers', '*')
-    next()
-  })
+  app.use(cors)
 }
